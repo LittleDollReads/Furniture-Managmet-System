cadffd784466a1919a3dd9355470f2c5f677c222
Marcus <152306593+GKMmarcus@users.noreply.github.com>
2024-05-02 16:02:43 -0400

    Add files via upload

---
+++ Sofa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectmain;
+
+/**
+ *
+ * @author marcu
+ */
+public class Sofa extends Furniture {
+    public Sofa(String materialType,String color,int price,double length,double width, double hieght){
+        super(materialType,color,price,length,width,hieght);
+    }
+    
+    @Override
+    public String toString(){
+        return "Material:" + materialType + " Color:" + color + " Price:" + price + "Dimensions=" + getDimensions().getLength() + "x" + getDimensions().getWidth() + "x" + getDimensions().getHeight();
+    }
+}
