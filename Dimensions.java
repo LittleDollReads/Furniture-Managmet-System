d143e917e2414e1365b05be087807b013d8f7fca
Marcus <152306593+GKMmarcus@users.noreply.github.com>
2024-05-02 17:13:39 -0400

    Update Dimensions.java

--- Dimensions.java
+++ Dimensions.java
@@ -7,31 +7,31 @@ package com.mycompany.projectmain;
 public class Dimensions {
     public double height,width,length;
     
-    public void getHeight(double height){
+    public void setHeight(double height){
         this.height = height;
     }
-    public double setHeight(){
+    public double getHeight(){
         return height;
     }
     
-    public void getWidth(double width){
+    public void setWidth(double width){
        this.width = width;
     }
-    public double setWidth(){
+    public double getWidth(){
         return width;
     }
     
-    public void getLength(double length){
+    public void setLength(double length){
         this.length = length;
     }
-    public double setLength(){
+    public double getLength(){
         return length;
     }
     
     public Dimensions(double length,double width,double height){
-        this.length = length;
-        this.width = width;
-        this.height = height;
+        setLength(length);
+        setWidth(width);
+        setHeight(height);
     }
 }
     
