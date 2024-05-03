a785a2c13c7d76e3ea0eda39838bf8c24a120b12
Emma Green <162496586+LittleDollReads@users.noreply.github.com>
2024-05-03 09:37:05 -0400

    Add files via upload

--- ObjectProject.java
+++ ObjectProject.java
@@ -13,8 +13,9 @@ public class ObjectProject {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         // TODO code application logic here
-    }
+        //FurnitureGUI.main(args);
+    //}
     
 }
