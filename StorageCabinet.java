4208cc719ff911616c39f598a81409080bdd884c
Emma Green <162496586+LittleDollReads@users.noreply.github.com>
2024-05-02 21:49:52 -0400

    Update StorageCabinet.java

--- StorageCabinet.java
+++ StorageCabinet.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.projectmain;
+package object.project;
 
 /**
  *
@@ -10,6 +10,8 @@ package com.mycompany.projectmain;
  */
 public class StorageCabinet extends Furniture {
     public int numofCabinet;
+    
+    public Dimensions StorageCabinetDimension;
     
     public void setnumofCabinet(int numofCabinet){
         this.numofCabinet = numofCabinet;
@@ -19,13 +21,18 @@ public class StorageCabinet extends Furniture {
         return numofCabinet;
     }
     
-    public StorageCabinet(String materialType,String color,int price,double length,double width, double hieght,int numofCabinet){
-        super(materialType,color,price,length,width,hieght);
+    public StorageCabinet(String materialType,String color,int price,double length,double width, double height,int numofCabinet){
+        super(materialType,color,price);
+        
+        StorageCabinetDimension = new Dimensions(length,width,height);
+        
         setnumofCabinet(numofCabinet);
     }
     
+    /**
     @Override
     public String toString(){
         return "Material:" + materialType + " Color:" + color + " Price:" + price +" Number of Cabinet:" + numofCabinet + " Dimensions=" + getDimensions().getLength() + "x" + getDimensions().getWidth() + "x" + getDimensions().getHeight();
-    }
+    }**/
+
 }
