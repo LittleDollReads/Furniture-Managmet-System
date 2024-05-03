cce15264b120f96a695518e84174a7b32d2de11d
Emma Green <162496586+LittleDollReads@users.noreply.github.com>
2024-05-02 21:48:29 -0400

    Update Sofa.java
    changed dimensions to work with the class.  added empty constructor.

--- Sofa.java
+++ Sofa.java
@@ -2,19 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.projectmain;
+package object.project;
 
 /**
  *
  * @author marcu
  */
 public class Sofa extends Furniture {
-    public Sofa(String materialType,String color,int price,double length,double width, double hieght){
-        super(materialType,color,price,length,width,hieght);
-    }
     
+    public Dimensions SofaDimension;
+    
+    public Sofa(String materialType,String color,int price,double length,double width, double height){
+        super(materialType,color,price);
+        
+        SofaDimension = new Dimensions(length, width, height);
+        
+    }
+    /**
     @Override
     public String toString(){
         return "Material:" + materialType + " Color:" + color + " Price:" + price + "Dimensions=" + getDimensions().getLength() + "x" + getDimensions().getWidth() + "x" + getDimensions().getHeight();
-    }
+    } */
 }
