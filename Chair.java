ea4e00cc68d8966b43e7325a24e9611461f19bfa
Emma Green <162496586+LittleDollReads@users.noreply.github.com>
2024-05-02 21:41:05 -0400

    Update Chair.java
    Corrected dimensions to use the class. Added empty constructor.

--- Chair.java
+++ Chair.java
@@ -2,19 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.projectmain;
+package object.project;
 
 /**
  *
  * @author marcu
  */
 public class Chair extends Furniture {
-    public Chair(String materialType,String color,int price,double length,double width, double hieght){
-        super(materialType,color,price,length,width,hieght);
+    
+    public Dimentions ChairDimention;
+    
+    public Chair(String materialType,String color,int price,double length,double width, double height){
+        super(materialType,color,price);
+        
+        ChairDimention = new Dimentions(length,width,height);
     }
     
+    public Chair(){
+        this("wood","brown",40, 12, 36,24);
+    }
+    
+    
+    /**
     @Override
     public String toString(){
         return "Material:" + materialType + " Color:" + color + " Price:" + price + "Dimensions:" + getDimensions().getLength() + "x" + getDimensions().getWidth() + "x" + getDimensions().getHeight();
     }
+    * **/
 }
